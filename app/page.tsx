b684660ed8c19269e354329b97bdc553e3dcd4af
ronit-ghosh <ronitghosh06@gmail.com>
2024-10-22 15:44:57 +0530

    Next App Created

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="h-dvh flex flex-col justify-center items-center">
+        <h3>Website under development</h3>
+        <span>You can check our works here</span>
+        <a className="underline cursor-pointer hover:text-orange-00 text-orange-500" href="https://ronit-ghosh.vercel.app/">Ronit</a>
+        <a className="underline cursor-pointer hover:text-orange-00 text-orange-500" href="https://sayak-dey.vercel.app/">Sayak</a>
+      </div>
+    </>
+  );
+}
