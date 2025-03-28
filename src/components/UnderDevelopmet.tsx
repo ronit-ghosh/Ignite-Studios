26154f8f808f1c132083b2863f4cf816b47e2840
ronit-ghosh <ronitghosh06@gmail.com>
2025-03-28 16:59:26 +0530

    Under Development

--- src/components/UnderDevelopmet.tsx
+++ src/components/UnderDevelopmet.tsx
@@ -4,7 +4,7 @@ export default function UnderDevelopmet() {
     return (
         <div className='w-full absolute mx-auto top-5 z-50'>
             <div className="w-44 mx-auto border rounded-2xl grid place-items-center py-2 uppercase">
-                Under Developmet    
+                Under Development    
             </div>
         </div>
     )
